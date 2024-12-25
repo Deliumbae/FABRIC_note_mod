14e26d37f5acc633a8fc9da3fda05d9f62ecb71b
23DP2DVeli <166109636+23DP2DVeli@users.noreply.github.com>
2024-12-25 15:52:18 +0200

    Initial commit

---
+++ src/main/java/net/doliv/note_mod/Note_modClient.java
@@ -0,0 +1,10 @@
+package net.doliv.note_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Note_modClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
